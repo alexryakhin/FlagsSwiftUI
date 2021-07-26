db7fb987bee67365176cd76da2194be38f2abdc8
Alex Bonney <bonney977@gmail.com>
2021-07-26 15:01:06 +0300

    Initial Commit

---
+++ FlagsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlagsSwiftUI
+//
+//  Created by Alexander Bonney on 7/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlagsSwiftUI/FlagsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsSwiftUIApp.swift
+//  FlagsSwiftUI
+//
+//  Created by Alexander Bonney on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct FlagsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
